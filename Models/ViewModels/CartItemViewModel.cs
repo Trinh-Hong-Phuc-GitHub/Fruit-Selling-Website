95df7cddddc1fcd32d6511653ff44a144946dbfd
Trinh-Hong-Phuc-Github <trinhhongphucwork@gmail.com>
2024-04-05 20:47:40 +0700

    BTL

---
+++ Models/ViewModels/CartItemViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FruitSellingWebsite.Models.ViewModels
+{
+    public class CartItemViewModel
+    {
+        public List<CartItemModel> CartItems { get; set; }
+        public decimal GrandTotal {  get; set; }
+    }
+}
---
+++ Repository/Components/CategoriesViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FruitSellingWebsite.Repository.Components
+{
+	public class CategoriesViewComponent: ViewComponent
+	{
+		private readonly DataContext _dataContext;
+		public CategoriesViewComponent(DataContext context)
+		{
+			_dataContext = context;
+		}
+		public async Task<IViewComponentResult> InvokeAsync() => View(await _dataContext.Categories.ToListAsync());
+	}
+}
